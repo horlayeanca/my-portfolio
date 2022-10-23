f93236cb7c7df34cb78288c763cb377bf8693acc
horlayeanca <adelodunfaruq@gmail.com>
2022-10-23 23:05:49 +0100

    .

--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -30,7 +30,9 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-neutral-200 shadow-sm fixed">
       <div>
-        <h1 className="text-5xl text-rose-700 font-signature ml-2">Horla</h1>
+        <h1 className="text-5xl text-rose-700 font-signature ml-2 lg:hover:scale-95 duration-300">
+          Horla
+        </h1>
       </div>
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
