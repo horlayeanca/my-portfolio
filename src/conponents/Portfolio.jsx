979adbc85b4f7543f491dcdc9078c47e1ea28680
horlayeanca <adelodunfaruq@gmail.com>
2022-10-28 12:40:36 +0100

    .

--- src/conponents/Portfolio.jsx
+++ src/conponents/Portfolio.jsx
@@ -49,9 +49,9 @@ const Portfolio = () => {
   return (
     <div
       name="Portfolio"
-      className="bg-neutral-100 w-full text-black md:h-screen lg:pt-40"
+      className="bg-neutral-100 w-full text-black md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full lg:pt-40">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8 text-center">
           <p className="text-4xl text-center font-bold inline border-b-4 border-gray-500">
             Portfolio
